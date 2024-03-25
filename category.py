e6850fe4d8fecf2f96a87a6f7d62e170ceb33d5a
mukezw <mukezwa@gmail.com>
2024-03-26 00:16:37 +0200

    feat: connect categories to the destinations_db

--- category.py
+++ category.py
@@ -1,3 +1,17 @@
+import mysql.connector
+
+# Connect to MySQL database
+conn = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='destinations_db',
+    auth_plugin='mysql_native_password'
+)
 class Category:
     def __init__(self, name):
         self.name = name
+
+# Close the connection when done
+def close_connection():
+    conn.close()
