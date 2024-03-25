8385c949046a6429a868864a802edd9603376107
mukezw <mukezwa@gmail.com>
2024-03-26 00:17:09 +0200

    feat: add query to save category to db

--- category.py
+++ category.py
@@ -8,9 +8,24 @@ conn = mysql.connector.connect(
     database='destinations_db',
     auth_plugin='mysql_native_password'
 )
+cursor = conn.cursor()
+
+# Create Category table if it doesn't exist
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE
+)
+''')
+conn.commit()
+
 class Category:
     def __init__(self, name):
         self.name = name
+
+    def save_to_db(self):
+        cursor.execute('INSERT INTO Category (name) VALUES (%s)', (self.name,))
+        conn.commit()
 
 # Close the connection when done
 def close_connection():
