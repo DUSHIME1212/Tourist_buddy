29f6c46e3a6c82e4b29daca1abfcfba50fdfcefa
mukezw <mukezwa@gmail.com>
2024-03-26 00:01:37 +0200

    feat: add user menu

---
+++ main.py
@@ -0,0 +1,14 @@
+def main_menu():
+    while True:
+        print("\n--- User Menu ---")
+        print("1. Explore Available Destinations")
+        print("2. Search Destination Name")
+        print("3. Filter Destination By Category")
+        print("4. Add New Attractions")
+        print("5. Add New Category")
+        print("6. Get Available Categories")
+        print("7. Get Recommended Places")
+        print("8. Calculate Destination Distance")
+        print("9. Exit")
+
+        choice = input("Enter your choice: ")
