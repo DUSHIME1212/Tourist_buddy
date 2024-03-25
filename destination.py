f88ad0b3d623e0c2dfa2e77a136805fd68000424
mukezw <mukezwa@gmail.com>
2024-03-26 00:19:17 +0200

    connect destination to db

--- destination.py
+++ destination.py
@@ -1,3 +1,14 @@
+import mysql.connector
+
+# Connect to MySQL database
+conn = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='destinations_db',
+    auth_plugin='mysql_native_password'
+)
+
 class Destination:
     def __init__(self, name, historical_background, operating_hours, exciting_facts, location, rating, key_nearby_places, category_id):
         self.name = name
@@ -8,3 +19,7 @@ class Destination:
         self.rating = rating
         self.key_nearby_places = key_nearby_places
         self.category_id = category_id
+
+# Close the connection when done
+def close_connection():
+    conn.close()
