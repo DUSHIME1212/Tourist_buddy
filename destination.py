14a9a5ba508ef317c8a55f1de9eb94b426555f07
mukezw <mukezwa@gmail.com>
2024-03-26 00:18:43 +0200

    create destination class

---
+++ destination.py
@@ -0,0 +1,10 @@
+class Destination:
+    def __init__(self, name, historical_background, operating_hours, exciting_facts, location, rating, key_nearby_places, category_id):
+        self.name = name
+        self.historical_background = historical_background
+        self.operating_hours = operating_hours
+        self.exciting_facts = exciting_facts
+        self.location = location
+        self.rating = rating
+        self.key_nearby_places = key_nearby_places
+        self.category_id = category_id
