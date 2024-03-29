fadf9659775ee794e0c9d2953950b1874e349209
mukezw <mukezwa@gmail.com>
2024-03-29 21:57:28 +0200

    check if destinations are empty

--- category.py
+++ category.py
@@ -4,8 +4,9 @@ import mysql.connector
 conn = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='12345678',
-    database='destination_db',
+    password='',
+    database='destinations_db',
+    auth_plugin='mysql_native_password'
 )
 cursor = conn.cursor()
 
--- viewdestinations.py
+++ viewdestinations.py
@@ -11,23 +11,25 @@ def explore_destinations():
     )
     cursor = connection.cursor()
 
-    #execute the select query 
-    cursor.execute("SELECT * FROM Destination")
+    cursor.execute('SELECT COUNT(*) FROM Destination')
+    num_destinations = cursor.fetchone()[0]
 
-    #fetch all rows
-    destinations = cursor.fetchall()
-    #printing details for the available destinations
-    for destination in destinations:
-        print('Destination Id:', destination[0])
-        print('Name:', destination[1])
-        print('Location:', destination[2])
-        print('Description:', destination[3])
-        print('------------------------------')
+    if num_destinations == 0:
+        print("There are no destinations available. Please consider adding some.")
+    else:
+        #execute the select query 
+        cursor.execute("SELECT * FROM Destination")
+
+        #fetch all rows
+        destinations = cursor.fetchall()
+        #printing details for the available destinations
+        for destination in destinations:
+            print('Destination Id:', destination[0])
+            print('Name:', destination[1])
+            print('Location:', destination[2])
+            print('Description:', destination[3])
+            print('------------------------------')
 
     #close the cursor and the connection
     cursor.close()
     connection.close()
-    print('MSQL connection is closed')
-
-#calling the explore function 
-explore_destinations()
