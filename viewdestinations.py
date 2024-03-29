d9aab1e44b38bf2d08232565af93bde30926d988
JDMUHIRWA <j.hareriman@alustudent.com>
2024-03-29 16:08:25 +0200

    fix linting

--- viewdestinations.py
+++ viewdestinations.py
@@ -1,33 +1,32 @@
 import mysql.connector
 
 def explore_destinations():
-    
-        #connecting my database
-            connection = mysql.connector.connect(
-                host = 'local host',
-                user = 'root',
-                password = 'Harerimana',
-                database = 'availdestination_db'
-            )
-            cursor = connection.cursor()
+    #connecting my database
+    connection = mysql.connector.connect(
+        host = 'local host',
+        user = 'root',
+        password = 'Harerimana',
+        database = 'availdestination_db'
+    )
+    cursor = connection.cursor()
 
-            #execute the select query 
-            cursor.execute("SELECT * FROM Destination")
+    #execute the select query 
+    cursor.execute("SELECT * FROM Destination")
 
-            #fetch all rows
-            destinations = cursor.fetchall()
-#printing details for the available destinations
-for destination in destinations:
-    print('Destination Id:', destination[0])
-    print('Name:', destination[1])
-    print('Location:', destination[2])
-    print('Description:', destination[3])
-    print('------------------------------')
+    #fetch all rows
+    destinations = cursor.fetchall()
+    #printing details for the available destinations
+    for destination in destinations:
+        print('Destination Id:', destination[0])
+        print('Name:', destination[1])
+        print('Location:', destination[2])
+        print('Description:', destination[3])
+        print('------------------------------')
 
-#close the cursor and the connection
-cursor.close()
-connection.close()
-print('MSQL connection is closed')
+    #close the cursor and the connection
+    cursor.close()
+    connection.close()
+    print('MSQL connection is closed')
 
 #calling the explore function 
 explore_destinations()
